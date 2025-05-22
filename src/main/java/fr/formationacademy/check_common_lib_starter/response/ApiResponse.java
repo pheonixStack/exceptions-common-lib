8b19d28240994af85a6239d9e9d337496f4f4ef2
pheonixStack <aitahmed.tscontact@gmail.com>
2025-05-22 21:43:47 +0100

    add Represents a standardized message used in API responses

--- src/main/java/fr/formationacademy/check_common_lib_starter/response/ApiResponse.java
+++ src/main/java/fr/formationacademy/check_common_lib_starter/response/ApiResponse.java
@@ -9,17 +9,6 @@ import java.util.List;
 
 /**
  * A generic class representing an API response.
- * 
- * This class is used to standardize the structure of API responses across the application.
- * It contains a status code, a data payload, and a list of messages.
- * 
- * @param <T> the type of the data in the response
- * 
- * @author Housseyn TALEB
- * @version 1.0
- * @since 2025-04-13
- * 
- * @see fr.formationacademy.check_common_lib_starter.response.ApiResponseBuilder
  */
 @Getter
 @Setter
